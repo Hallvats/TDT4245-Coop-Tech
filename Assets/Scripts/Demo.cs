43af30ce2949f25e43c81c9c75b68bb8d6f99c38
Hallvard <hallvats@stud.ntnu.no>
2020-03-16 17:04:59 +0100

    Instantiate cards from PNGs

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class Demo : MonoBehaviour
+{
+
+		//public List<GameObject> shields;
+		public List<GameObject> hand;
+		//public Stack<GameObject> energyDeck;
+		//public Stack<GameObject> actionDeck;
+		//public Stack<GameObject> discardPile;
+    // Start is called before the first frame update
+    void Start()
+    {
+			DirectoryInfo dir = new DirectoryInfo("Assets/Resources/Cards/Action/Offense");
+			FileInfo[] info = dir.GetFiles("*.png");
+			foreach(FileInfo f in info) {
+				GameObject newCard = Instantiate(Resources.Load("Prefabs/Card")) as GameObject;
+				newCard.GetComponent<SpriteRenderer>().sprite = (Sprite) AssetDatabase.LoadAssetAtPath("Assets" + f.FullName.Substring(Application.dataPath.Length), typeof(Sprite));
+				hand.Add(newCard);
+			}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
